0598bfa9744e99689653297bb3d2fd437b525bd6
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-03-29 11:50:57 +0100

    Exercice 6

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,22 +1,50 @@
 import PokemonCard from './components/PokemonCard'
+import { useState } from 'react'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const PreviousPok = () => {
+  if (count>0)  {setCount(count - 1)}
+}
+
+  const NextPok = () => {
+    if (count < pokemonList.length - 1) {setCount(count + 1)}
+  }
+
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[count]}/>
+      <button onClick={PreviousPok}>Précédant</button>
+      <button onClick={NextPok}>Suivant</button>
     </div>
   );
 }
