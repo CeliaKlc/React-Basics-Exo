59675c31ceace4984613e2bdaa501d9419ed988c
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-03-29 11:58:37 +0100

    Modification exo

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,4 +1,7 @@
 import PokemonCard from './components/PokemonCard'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import { useState } from 'react'
 
 const pokemonList = [
