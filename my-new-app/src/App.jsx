73ff8d5c0127e88636bb00f990be18efe4e2d13e
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-03-31 16:12:35 +0200

    Exercice 8

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,46 +1,47 @@
-import PokemonCard from './components/PokemonCard'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useState } from 'react'
-import NavBar from './Components/NavBar'
+import PokemonCard from "./components/PokemonCard";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useState } from "react";
+import NavBar from "./Components/NavBar";
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const pokemon = pokemonList[setCount];
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[count]}/>
+      <PokemonCard pokemon={pokemonList[count]} />
       <NavBar count={count} setCount={setCount} pokemonList={pokemonList} />
     </div>
   );
 }
 
-export default App
+export default App;
--- my-new-app/src/Components/NavBar.jsx
+++ my-new-app/src/Components/NavBar.jsx
@@ -1,25 +1,17 @@
-function Navbar({pokemonList, count, setCount}) {
-
-    const PreviousPok = () => {
-        if (count > 0)  {
-            setCount(count - 1)
-        }
-    }
-
-    const NextPok = () => {
-        if (count < pokemonList.length - 1) {
-            setCount(count + 1)
-        }
-    }
-
-
-    return (
-        <nav>
-          <button onClick={PreviousPok}>Précédant</button>
-          <button onClick={NextPok}>Suivant</button>
-        </nav>
-      );
-};
-
+function Navbar({ pokemonList, setCount }) {
+  return (
+    <nav>
+      {pokemonList.map((pokemon, index) => (
+        <button
+          type="button"
+          key={pokemon.name}
+          onClick={() => setCount(index)}
+        >
+          {pokemon.name}
+        </button>
+      ))}
+    </nav>
+  );
+}
 
 export default Navbar;
